8de0d6a18e86acbe69c7132550bc24876e03df30
clefego <70776298+clefego@users.noreply.github.com>
2020-10-06 18:24:03 -0300

    teste

---
+++ lib/common/Constants.dart
@@ -0,0 +1,1 @@
+final apikey = "36da7e51c9610833f4d503edcf8628f1";
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/common/HttpHandler.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +7,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    _loadJson();
+  }
+
+  _loadJson() async {
+    String data = await HttpHandler().fetchMovies();
+    print(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
