b37d0a5b8663cd942d57bcd27466a0fdb02ccc34
clefego <70776298+clefego@users.noreply.github.com>
2020-10-13 16:14:00 -0300

    Update media_list_item.dart

--- lib/media_list_item.dart
+++ lib/media_list_item.dart
@@ -9,14 +9,29 @@ class MediaListItem extends StatelessWidget {
     return new Card(
       child: new Column(
         children: <Widget>[
-          new FadeInImage.assetNetwork(
-            placeholder: "assets/placeholder.jpg",
-            image: media.getBackDropUrl(),
-            fit: BoxFit.cover,
-            fadeInDuration: new Duration(milliseconds: 40),
-            width: double.infinity,
-            height: 200.0,
-          )
+          new Container(
+              child: new Stack(
+            children: <Widget>[
+              new FadeInImage.assetNetwork(
+                placeholder: "assets/placeholder.jpg",
+                image: media.getBackDropUrl(),
+                fit: BoxFit.cover,
+                fadeInDuration: new Duration(milliseconds: 40),
+                width: double.infinity,
+                height: 200.0,
+              ),
+              new Positioned(
+                left: 0.0,
+                bottom: 0.0,
+                right: 0.0,
+                child: new Container(
+                  decoration: new BoxDecoration(
+                      color: Colors.grey[900].withOpacity(0.5)),
+                  constraints: new BoxConstraints.expand(height: 55.0),
+                ),
+              )
+            ],
+          ))
         ],
       ),
     );
